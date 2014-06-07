550e5f73dffcd4bb419b4f5bb0fe66e70f210df5
Hermes Pique <hermespique@Hermess-MacBook-Pro.local>
2014-06-07 11:51:21 -0700

    Add Settings View Controller

---
+++ InterfaCSSStatusBarBlurryTableViewController/IKJSettingsViewController.m
@@ -0,0 +1,49 @@
+//
+//  IKJSettingsViewController.m
+//  InterfaCSSStatusBarBlurryTableViewController
+//
+//  Created by Hermes Pique on 6/7/14.
+//  Copyright (c) 2014 PodRoulette. All rights reserved.
+//
+
+#import "IKJSettingsViewController.h"
+
+@interface IKJSettingsViewController ()
+
+@end
+
+@implementation IKJSettingsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
