04174ff153f0a827c3db575c951b9ed3ee3555aa
aminebellakrid <a.bellakrid@gmail.com>
2014-06-07 12:07:46 -0700

    Adding select image to change blurred images

---
+++ InterfaCSSStatusBarBlurryTableViewController/IKJChatRoomTableViewCell.m
@@ -0,0 +1,34 @@
+//
+//  IKJChatRoomTableViewCell.m
+//  InterfaCSSStatusBarBlurryTableViewController
+//
+//  Created by Amine Bellakrid on 07/06/14.
+//  Copyright (c) 2014 PodRoulette. All rights reserved.
+//
+
+#import "IKJChatRoomTableViewCell.h"
+
+@implementation IKJChatRoomTableViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
--- InterfaCSSStatusBarBlurryTableViewController/IKJChatRoomViewController.m
+++ InterfaCSSStatusBarBlurryTableViewController/IKJChatRoomViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "IKJChatRoomViewController.h"
+#import "IKJChatRoomTableViewCell.h"
 
 @interface IKJChatRoomViewController ()
 
@@ -47,5 +48,74 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+
+#pragma mark - UITableView delegate
+
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    IKJChatRoomTableViewCell *cell = (IKJChatRoomTableViewCell *)[tableView cellForRowAtIndexPath:indexPath];
+    NSArray *images = @[@"balloons",@"tiger",@"whaou"];
+    NSString *imageName = [images objectAtIndex:indexPath.row];
+    [self configureBannerWithImage:[UIImage imageNamed:imageName] height:200 blurRadius:12 blurTintColor:[UIColor colorWithWhite:0 alpha:0.5] saturationFactor:1];
+    [cell setSelected:YES animated:YES];
+}
+
+
+#pragma mark - UITableView data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    IKJChatRoomTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"IKJChatRoomTableViewCell" forIndexPath:indexPath];
+    NSArray *images = @[@"balloons",@"tiger",@"whaou"];
+    NSString *imageName = [images objectAtIndex:indexPath.row];
+    cell.blurredProfileImage.image = [self blur:[UIImage imageNamed:imageName]];
+
+    return cell;
+}
+
+- (UIImage*) blur:(UIImage*)theImage
+{
+    // ***********If you need re-orienting (e.g. trying to blur a photo taken from the device camera front facing camera in portrait mode)
+    // theImage = [self reOrientIfNeeded:theImage];
+    
+    // create our blurred image
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CIImage *inputImage = [CIImage imageWithCGImage:theImage.CGImage];
+    
+    // setting up Gaussian Blur (we could use one of many filters offered by Core Image)
+    CIFilter *filter = [CIFilter filterWithName:@"CIGaussianBlur"];
+    [filter setValue:inputImage forKey:kCIInputImageKey];
+    [filter setValue:[NSNumber numberWithFloat:15.0f] forKey:@"inputRadius"];
+    CIImage *result = [filter valueForKey:kCIOutputImageKey];
+    
+    // CIGaussianBlur has a tendency to shrink the image a little,
+    // this ensures it matches up exactly to the bounds of our original image
+    CGImageRef cgImage = [context createCGImage:result fromRect:[inputImage extent]];
+    
+    UIImage *returnImage = [UIImage imageWithCGImage:cgImage];//create a UIImage for this function to "return" so that ARC can manage the memory of the blur... ARC can't manage CGImageRefs so we need to release it before this function "returns" and ends.
+    CGImageRelease(cgImage);//release CGImageRef because ARC doesn't manage this on its own.
+    
+    return returnImage;
+    
+    // *************** if you need scaling
+    // return [[self class] scaleIfNeeded:cgImage];
+}
+
+
 
 @end
